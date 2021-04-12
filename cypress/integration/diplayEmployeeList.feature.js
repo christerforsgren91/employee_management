ea5ca7fbf5e354bacea1f86dce9ed5bf5cfcb9e3
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-12 13:43:48 +0200

    adds employee list render and test, hardcoded list of employees in state

--- cypress/integration/diplayEmployeeList.feature.js
+++ cypress/integration/diplayEmployeeList.feature.js
@@ -6,4 +6,13 @@ describe('Display list of employees', () => {
   it('display a header', () => {
     cy.get('[data-cy=header]').should('contain', 'Employee List')
   })
+
+  it('renders employee list properly', () => {
+    cy.get('[data-cy=employee-list]').within(() => {
+      cy.get('[data-cy=employee-item]')
+        .first()
+        .find('[data-cy=name]')
+        .should('contain', 'George Bluth')
+    })
+  })
 })
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import EmployeeList from './EmployeeList'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1 data-cy='header'>Employee List</h1>
+        <EmployeeList />
       </div>
     )
   }
